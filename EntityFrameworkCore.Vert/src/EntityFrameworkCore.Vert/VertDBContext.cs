fc5af8b5183ec9f15675558d0973980c717a7c95
lenny <1941973283@qq.com>
2017-01-07 17:28:37 +0800

    init

---
+++ EntityFrameworkCore.Vert/src/EntityFrameworkCore.Vert/VertDBContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore.Vert
+{
+    public class VertDBContext : DbContext
+    {
+        private List<string> connectionStringList;
+
+        private int connIndex = 0;
+
+        public VertDBContext()
+        {
+
+        }
+
+        public VertDBContext(int index)
+        {
+            connIndex = index;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionStringList[connIndex]);
+        }
+    }
+}
